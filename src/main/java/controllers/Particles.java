1470061596f3bbd9ccffee598cf443b0ce0c4bed
Dabi2003 <91423@farnborough.ac.uk>
2020-10-15 15:30:26 +0100

    12/10/2020 Bug in Particles.java 12/10/2020 Nearly finished Particle/get API

--- src/main/java/controllers/Particles.java
+++ src/main/java/controllers/Particles.java
@@ -21,8 +21,8 @@ import java.sql.SQLException;
 public class Particle {
     @GET
     @Path("get")
-    pubilc  String Particles(@FormDataParam("ParticleID") Integer ParticleID, @FormDataParam("Name") String Name, @FormDataParam("Radius") Integer Radius, @FormDataParam("Charge") Integer Charge,
-                             @FormDataParam("Symbol") Character Symbol,@FormDataParam("Anti-Particle") S){
+    pubilc  String Particles(@FormDataParam("ParticleID") Integer ParticleID, @FormDataParam("Name") String Name, @FormDataParam("Symbol") Character Symbol, @FormDataParam("Antiparticle") String Antiparticle,
+                             @FormDataParam("Charge") Integer Charge,@FormDataParam("Radius")Integer Radius,@FormDataParam("Antisymbol")String Antisymbol,@FormDataParam("Description")String Description){
         System.out.println("Invoked Particles.GetParticle() with ParticleID " +ParticleID);
         try{
             PreparedStatement ps=Main.db.prepareStatement("SELECT Name,Radius,Charge FROM Particles WHERE ParticleID=?");
@@ -32,10 +32,15 @@ public class Particle {
             if (results.next()== true) {
                 response.put("ParticleID", ParticleID);
                 response.put("Name", results.getString(1));
-
-
+                response.put("Symbol",results.getCharacterStream(2));
+                response.put("Antiparticle",results.getString(3));
+                response.put("Charge",results.getInt(4));
+                response.put("Radius",results.getInt(5));
+                response.put("Antisymbol",results.getInt(6));
+                response.put("Description",results.getString(7));
             }
 
+            
         } catch (SQLException e) {
             e.printStackTrace();
         }
