1789de191cf3a5fc86d54f8f7c3517d3500628d2
Dabi2003 <91423@farnborough.ac.uk>
2020-11-02 13:48:00 +0000

    API Particle/save done and Activity/get done. Problem is that database is not allowing me to declare my ActivityID as an integer

---
+++ src/controllers/Activites.java
@@ -0,0 +1,41 @@
+package controllers;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import server.Main;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@Path("Activity/") //started with the Path parameter called 'Activity'
+@Consumes(MediaType.MULTIPART_FORM_DATA)
+@Produces(MediaType.APPLICATION_JSON)
+public class Activites{
+    @GET
+    @Path("get/{ActivityID}")   //created path to read Activity record
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public  String GetActivity(@PathParam("ActivityID") Integer ActivityID){
+        System.out.println("Invoked Activites.GetActivity() with ActivityID" + ActivityID);
+        try{
+            PreparedStatement ps = Main.db.prepareStatement("SELECT Title,URL,Description FROM Activites WHERE ActivityID=?");
+            ps.setInt(1,ActivityID);
+            ResultSet results = ps.executeQuery();
+            JSONObject response = new JSONObject();
+            if(results.next()==true){
+                response.put("ActivityID",ActivityID);
+                response.put("Title",results.getString(1));
+                response.put("URL",results.getString(2));
+                response.put("Description",results.getString(3));
+            } else{
+                System.out.println("Error");
+            }
+            return response.toString();
+
+        }catch (Exception exception) {
+            System.out.println("Database error:" + exception.getMessage());
+            return "{\"Error\": \"Unable to get item, please see server console for more info.\"}";
+        }
+    }
+
+}
--- src/controllers/Particles.java
+++ src/controllers/Particles.java
@@ -1,6 +1,7 @@
 package controllers;
 
 
+import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import server.Main;
@@ -9,13 +10,14 @@ import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+
 @Path("Particles/") //started with the Path parameter ttttttttt
 @Consumes(MediaType.MULTIPART_FORM_DATA)
 @Produces(MediaType.APPLICATION_JSON)
 
 public class Particles{            //declaring particles class
     @GET
-    @Path("get/{ParticleID}")   //created a path to read
+    @Path("get/{ParticleID}")   //created a path to read particle record
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.APPLICATION_JSON)
     public  String GetParticle(@PathParam("ParticleID") Integer ParticleID){
@@ -45,8 +47,8 @@ public class Particles{            //declaring particles class
     }
     @GET
     @Path("list")
-    public String Particlelist(){
-        System.out.println("Invoked Particles.Particlelist()");
+    public String ParticleList(){
+        System.out.println("Invoked Particles.ParticleList()");
         JSONArray response= new JSONArray();
         try{
             PreparedStatement ps=Main.db.prepareStatement("SELECT Name,Charge,Description From Particles");
@@ -54,7 +56,7 @@ public class Particles{            //declaring particles class
             while (results.next()==true){
                 JSONObject row = new JSONObject();
                 row.put("Name",results.getString(1));
-                row.put("Charge",results.getString(2));
+                row.put("Charge",results.getInt(2));
                 row.put("Description",results.getString(3));
                 response.add(row);
             }
@@ -66,6 +68,31 @@ public class Particles{            //declaring particles class
 
         }
     }
+    @POST
+    @Path("save")  //Creating a POST request with Path called "save" so resultant path would be Particles/save
+     public  String ParticleSave(@FormDataParam("ParticleID") Integer ParticleID,@FormDataParam("Name") String Name,@FormDataParam("Symbol") String Symbol,@FormDataParam("AntiParticle") String AntiParticle,@FormDataParam("Charge") Integer Charge,@FormDataParam("Radius") Integer Radius,@FormDataParam("Antisymbol") String AntiSymbol,@FormDataParam("Description") String Description,@CookieParam("Token") String Token){
+        System.out.println("Invoked Particles.ParticleSave");//Adding my form data parameters which are all the fields in the particles table along with cookie
+        try{
+            PreparedStatement ps=Main.db.prepareStatement("INSERT INTO Particles (ParticleID,Name,Symbol,AntiParticle,Charge,Radius,AntiSymbol,Description) VALUES");
+            ps.setInt(1,ParticleID);
+            ps.setString(2,Name);
+            ps.setString(3,Symbol);
+            ps.setString(4,AntiParticle); //Setting the values for the fields in to a new record to values of form data parameters
+            ps.setInt(5,Charge);
+            ps.setInt(6,Radius);
+            ps.setString(7,AntiSymbol);
+            ps.setString(8,Description);
+            ps.execute();
+            return "{\"OK\": \"Added user.\"}";
+        }  catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to create new item, please see server console for more info.\"}";
+        }
+
+
+    }
+
+
 
 
 
