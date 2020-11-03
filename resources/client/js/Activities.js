d0607d17367fe84e38e1aa2b7b04cb09bd5cfd6b
Dabi2003 <91423@farnborough.ac.uk>
2020-11-03 16:02:31 +0000

    3/11/2020 The Activity/get API is fixed and the first page the "Menu" page is under development. The Pair Production button is created. When clicked it fetches the URL for the correct web page.

---
+++ resources/client/js/Activities.js
@@ -0,0 +1,21 @@
+function getPP(){  // declaring a function to fetch the "Pair Production web page
+    console.log("Invoked getPP()");  // A test to see if the function was actually called
+    let ActivityID=1; //declaring the ActivityID to 1 which is the same ActivityID for the record in the database
+    //debugger;
+    const url="/Activity/get/";// this is the first part of the API path to get the record
+    fetch(url+ActivityID,{ // The API is called and the record is specified
+        method:"GET", // declaring that it's a get method
+    }).then(response=>{
+        return response.json(); // returns the record as JSON
+    }).then(response=>{
+        if(response.hasOwnProperty("Error")){  // Checks if web browser has an error
+            alert(JSON.stringify(response)); //If there is convert JSON Object to a string
+        } else{
+            console.log(response.URL); // this is a test to see if the correct URL was fetched.
+        }
+
+    });
+
+
+
+}
