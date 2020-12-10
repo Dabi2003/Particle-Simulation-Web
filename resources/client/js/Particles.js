2607d05f6dc4595e95abd87ca82752309ed592f7
Dabi2003 <91423@farnborough.ac.uk>
2020-12-10 16:12:16 +0000

    10/12/2020 API PARTICLE.GET WORKS PARTICLE MOVES SPEED CAN BE CONTROLLED.

--- resources/client/js/Particles.js
+++ resources/client/js/Particles.js
@@ -1,7 +1,36 @@
 let canvas;
 let ctx;
 let Proton;
+let p1;
+let check=0;
+function getParticle(){
+    console.log("Invoked getParticle()");
+    const Name= document.getElementById("Particle").value;
+    const Speed=document.getElementById("speed").value;
+    const url="/Particles/get/";
+    fetch(url+Name,{
+        method:"GET",
+    }).then(response=>{
+        return response.json();
+    }).then(response=>{
+        if(response.hasOwnProperty("Error")){
+            alert(JSON.stringify(response));
+        } else{
+            p1= new Particle(10,300,parseFloat(Speed),0.0,response.Radius,"blue",response.Symbol);
+            console.log(p1.radius);
+            console.log(p1.vx);
+            check=1;
+            return(check);
 
+
+
+        }
+
+    })
+
+
+
+}
 function pageLoad() {
 
     canvas = document.getElementById("canvas"); //capturing canvas element so that canvas may be used.
@@ -10,13 +39,13 @@ function pageLoad() {
     canvas.width = 800; //width of canvas modified due to canvas initially small
     canvas.height = 600;// height of canvas modified due to canvas initially small
 
-    Proton= new Particle( 10, 300,3,0,8,"yellow");
+    Proton= new Particle( 10, 300,3,0,8,"yellow","p"); //proton particle created
 
-    draw();
+
 
 }
 
-function Particle(px, py, vx, vy, radius, colour) {
+function Particle(px, py, vx, vy, radius, colour,symbol) {    // class named particles t take values of the x and y position, radius and colour
     this.px = px;
     this.py = py;
     this.vx = vx;
@@ -27,18 +56,21 @@ function Particle(px, py, vx, vy, radius, colour) {
     }
     this.radius = radius;
     this.colour = colour;
-    this.mass = 1;
+    this.mass = 1; // mass of particles is one.
+    this.symbol=symbol;
 
-    this.load = function () {
+    this.load = function () { //method to load the particle on screen
         ctx.beginPath();
         ctx.arc(this.px, this.py, this.radius, 0, 2 * Math.PI);
-        ctx.fillStyle = this.colour;
+        ctx.fillStyle = "white";
+        ctx.fillText(symbol,this.px+10,this.py+5); //positioning the symbol
+        ctx.fillStyle=this.colour;
         ctx.fill();
     }
 
 
 
-    this.move = function () {
+    this.move = function () { //method to for the particle to move and deflect from walls.
         this.px += this.velocity.x;
         this.py += this.velocity.y;
         if (this.px < 0 || this.px > canvas.width) {
@@ -52,15 +84,26 @@ function Particle(px, py, vx, vy, radius, colour) {
     }
 }
 
-function draw() {
+function draw() { //main function runs the canvas animation
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    Proton.load();
-    Proton.move();
+    p1.vx=0;
+    p1.load();
+    p1.move();
     window.requestAnimationFrame(draw);
 
 }
 
-function goHome(){
+function goHome(){ //returning to main menu
     window.open("http://localhost:8081/client/Menu.html")
+}
+function Start(){
+    getParticle(check);
+   if(check==1){
+       animate()
+       console.log(check);
+    }
+}
+function animate(){
+    draw();
 }
